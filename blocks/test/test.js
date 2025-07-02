f62029589893ef6d977d9d47f466996777ef46b9
abeheram <74228542+abeheram@users.noreply.github.com>
2025-07-02 14:43:13 +1000

    Update test.js

--- blocks/test/test.js
+++ blocks/test/test.js
@@ -3,11 +3,13 @@ export default function decorate(block) {
     if (firstDiv) {
       firstDiv.classList.add("headertest"); // Add the class 'headertet' to the first <div>
     }
+  const divs = block.querySelectorAll("div");
   if (
       divs.length >= 2 &&
       divs[0].querySelector("p") &&
       divs[1].querySelector("p.button-container")
     ) {
       const firstDivParagraph = divs[0].querySelector("p")?.textContent || "";
+      console.log(firstDivParagraph);
     }
 }
